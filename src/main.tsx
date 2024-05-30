fbf6cba4a7328b1b517754203fc4e3f09f0a2870
Egor Ryazanov <egor.ryazanov@saritasa.com>
2024-05-31 02:15:55 +0500

    Add env file for development

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from './app/providers/ErrorBoundary/ui/ErrorBoundary.tsx';
 import { CssBaseline } from '@mui/material';
 import ThemeProvider from './app/providers/ThemeProvider/ThemeProvider.tsx';
-import { StoreProvider } from './app/providers/StoreProvider/ui/StoreProvider.tsx';
+import { StoreProvider } from './app/providers/StoreProvider/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
