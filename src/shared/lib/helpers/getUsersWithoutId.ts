c9a0c5bce1bf6d46b15d8b748fa6c42542986372
nchertolin <nchertolin@gmail.com>
2024-07-23 01:13:43 +0500

    refactor: remove unused  eslint-disable directive

--- src/shared/lib/helpers/getUsersWithoutId.ts
+++ src/shared/lib/helpers/getUsersWithoutId.ts
@@ -7,7 +7,6 @@ interface User {
 
 export const getUsersWithoutId = (users: User[]) => {
     return users.map(user => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         return { ...user, id: undefined };
     });
 };
