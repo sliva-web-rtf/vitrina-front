4edb92056a9a9843f067e9a47439ed5fcdbe4fa0
Nihat04 <119858790+Nihat04@users.noreply.github.com>
2025-01-23 01:14:16 +0500

    feat: create dashboard block

---
+++ src/shared/ui/DashboardBlock/DashboardBlock.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+import { Box, Typography } from '@mui/material';
+import { VStack } from '../Stack/Stack';
+
+interface DashboardBlockProps {
+    title: string;
+    subtitle?: ReactNode;
+    children?: ReactNode;
+}
+
+export const DashboardBlock = (props: DashboardBlockProps) => {
+    const { title, subtitle, children } = props;
+
+    return (
+        <Box sx={{ margin: '0 auto', maxWidth: '1544px' }}>
+            <VStack
+                spacing={'32px'}
+                sx={(theme) => ({
+                    marginBottom: '96px',
+                    [theme.breakpoints.down('lg')]: {
+                        marginBottom: '32px',
+                    },
+                })}
+            >
+                <Typography variant="h2">{title}</Typography>
+                {subtitle && (
+                    <Typography
+                        sx={(theme) => ({
+                            fontSize: '32px',
+                            [theme.breakpoints.down('xl')]: {
+                                fontSize: '22px',
+                            },
+                            [theme.breakpoints.down('sm')]: {
+                                fontSize: '16px',
+                            },
+                        })}
+                    >
+                        {subtitle}
+                    </Typography>
+                )}
+            </VStack>
+            <Box>{children}</Box>
+        </Box>
+    );
+};
