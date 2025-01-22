9f67b4c802a7f6cd2dcd078c3c93ed9a377664e9
Nihat04 <119858790+Nihat04@users.noreply.github.com>
2025-01-23 01:12:03 +0500

    fix: accordion mobile adaptive

--- src/shared/ui/Accordion/Accordion.tsx
+++ src/shared/ui/Accordion/Accordion.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import React, { ReactNode } from 'react';
-import { Accordion as AccordionBase, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import { Accordion as AccordionBase, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material';
 
 import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
 
@@ -27,11 +29,37 @@ export const Accordion = (props: AccordionProps) => {
                 id="panel1-header"
                 sx={{ padding: '16px 24px', border: 'none' }}
             >
-                <Typography component="span" sx={{ fontSize: '32px', fontWeight: 700 }}>
+                <Typography
+                    component="span"
+                    sx={(theme) => ({
+                        fontSize: '32px',
+                        fontWeight: 700,
+                        [theme.breakpoints.down('xl')]: {
+                            fontSize: '28px',
+                        },
+                        [theme.breakpoints.down('sm')]: {
+                            fontSize: '24px',
+                        },
+                    })}
+                >
                     {label}
                 </Typography>
             </AccordionSummary>
-            <AccordionDetails>{children}</AccordionDetails>
+            <AccordionDetails>
+                <Box
+                    sx={(theme) => ({
+                        fontSize: '32px',
+                        [theme.breakpoints.down('xl')]: {
+                            fontSize: '22px',
+                        },
+                        [theme.breakpoints.down('sm')]: {
+                            fontSize: '16px',
+                        },
+                    })}
+                >
+                    {children}
+                </Box>
+            </AccordionDetails>
         </AccordionBase>
     );
 };
