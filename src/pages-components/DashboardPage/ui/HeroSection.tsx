30a6220d84a849d24247bc955b14f0e52a97b043
Nihat04 <119858790+Nihat04@users.noreply.github.com>
2025-01-26 23:46:56 +0500

    fix: separated tags

--- src/pages-components/DashboardPage/ui/HeroSection.tsx
+++ src/pages-components/DashboardPage/ui/HeroSection.tsx
@@ -5,10 +5,12 @@ import { CompactHero } from '@/widgets/CompactHero';
 const HERO_TITLE =
     'Проектный практикум — это образовательная форма, которая сочетает теоретическое обучение с практическим применением знаний и навыков в реальных или симулированных проектах';
 
+const HERO_TAGS = ['ИРИТ-РТФ', 'ИНФО', 'Проектное обучение'];
+
 export const HeroSection = () => {
     return (
         <section style={{ marginTop: '0' }}>
-            <CompactHero title={HERO_TITLE} tags={['ИРИТ-РТФ', 'ИНФО', 'Проектное обучение']} />
+            <CompactHero title={HERO_TITLE} tags={HERO_TAGS} />
         </section>
     );
 };
