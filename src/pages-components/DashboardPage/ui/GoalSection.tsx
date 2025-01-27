fad8685dfdd12f007083de9098fea7cf88f8de0e
Nihat04 <119858790+Nihat04@users.noreply.github.com>
2025-01-28 00:21:22 +0500

    fix: code refactor

--- src/pages-components/DashboardPage/ui/GoalSection.tsx
+++ src/pages-components/DashboardPage/ui/GoalSection.tsx
@@ -4,6 +4,16 @@ import React from 'react';
 
 import { BarChart } from '@/shared/ui/BarChart/BarChart';
 import { DashboardBlock } from '@/shared/ui/DashboardBlock/DashboardBlock';
+import { Typography } from '@mui/material';
+
+const TITLE = 'Основная цель'
+
+const SUBTITLE = 'Мы хотим предоставить студентам возможность применить полученные знания на практике, ';
+const SUBTITLE_COLORED = (
+    <span className={styles['colorBlueText']}>
+        развить навыки командной работы, управления проектами и попробовать себя в решении реальных задач.
+    </span>
+);
 
 const CHART_DATA = [
     { label: '2 курс', data: 950, backgroundColor: 'rgba(0, 123, 255, 1)' },
@@ -11,21 +21,13 @@ const CHART_DATA = [
     { label: '4 курс', data: 500, backgroundColor: 'rgba(173, 216, 230, 1)' },
 ];
 
+
 export const GoalSection = () => {
     return (
         <section id="goal">
-            <DashboardBlock
-                title="Основаня цель"
-                subtitle={
-                    <>
-                        Мы хотим предоставить студентам возможность применить полученные знания на практике,{' '}
-                        <span className={styles['colorBlueText']}>
-                            развить навыки командной работы, управления проектами и попробовать себя в решении реальных
-                            задач.
-                        </span>
-                    </>
-                }
-            >
+            <DashboardBlock title={TITLE} subtitle={<Typography>
+                {SUBTITLE}{SUBTITLE_COLORED}
+            </Typography>}>
                 <BarChart data={CHART_DATA} />
             </DashboardBlock>
         </section>
