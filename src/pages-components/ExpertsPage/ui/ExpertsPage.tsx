902d0c59c0efff93a3c3e2fdeae715bb7f7f9638
Nihat04 <aliev.nihat4@gmail.com>
2025-02-05 16:09:54 +0500

    fix!: fixed horizontal scroll on main page
    IMPORTANT CHANGES:
    - remove "position: relative" from body element
    - remove "position: relative" from main element
    
    Mentions:
    - element round after hero now applied by hero itself

--- src/pages-components/ExpertsPage/ui/ExpertsPage.tsx
+++ src/pages-components/ExpertsPage/ui/ExpertsPage.tsx
@@ -2,12 +2,11 @@ import { VStack } from '@/shared/ui';
 import { ExpertsShowcase } from '@/widgets/ExpertsShowcase';
 import { ExpertsHero } from '@/widgets/Hero';
 import { MainHeader } from '@/widgets/MainHeader';
-import { Showcase } from '@/widgets/Showcase';
 import React from 'react';
 
 const ExpertsPage = () => {
     return (
-        <VStack spacing={6}>
+        <VStack>
             <MainHeader />
             <ExpertsHero />
             <ExpertsShowcase />
