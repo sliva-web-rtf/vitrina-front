ad56d3c875c7a9e3f975ba8c54621793ae0d6699
Nihat04 <aliev.nihat4@gmail.com>
2025-02-07 03:50:41 +0500

    fix: experts page

--- src/pages-components/ExpertsPage/ui/ExpertsPage.tsx
+++ src/pages-components/ExpertsPage/ui/ExpertsPage.tsx
@@ -2,14 +2,12 @@ import { VStack } from '@/shared/ui';
 import { ExpertsHeader } from '@/widgets/ExpertsHeader';
 import { ExpertsShowcase } from '@/widgets/ExpertsShowcase';
 import { ExpertsHero } from '@/widgets/Hero';
-import { MainHeader } from '@/widgets/MainHeader';
-import { Showcase } from '@/widgets/Showcase';
 import React from 'react';
 
 const ExpertsPage = () => {
     return (
-        <VStack spacing={6}>
-            <MainHeader />
+        <VStack>
+            <ExpertsHeader />
             <ExpertsHero />
             <ExpertsShowcase />
         </VStack>
