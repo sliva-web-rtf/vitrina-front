f32cebb9888cb5f77e0206c64804b83334c7ea4f
Nihat04 <aliev.nihat4@gmail.com>
2025-02-12 15:24:23 +0500

    fix: update expert card

--- src/widgets/ExpertsShowcase/ui/ExpertCard.tsx
+++ src/widgets/ExpertsShowcase/ui/ExpertCard.tsx
@@ -12,18 +12,19 @@ interface ExpertCardProps {
 
 export const ExpertCard = (props: ExpertCardProps) => {
     const { expert } = props;
+    const { name, avatarUrl, workCompany, workPosition } = expert;
 
     return (
         <VStack gap={1} className={`${styles['expertCard']} ${styles['stack']}`}>
             <Box>
-                <BaseAvatar src={expert.avatarUrl} alt={`Изображение ${expert.name}`} className={styles['avatar']} />
+                <BaseAvatar src={avatarUrl} alt={`Изображение ${name}`} className={styles['avatar']} />
             </Box>
             <VStack className={styles['stack']}>
                 <Typography variant="h4" color={'var(--primary-color-mono)'} className={styles['mainText']}>
-                    {expert.name}
+                    {name}
                 </Typography>
                 <Typography variant="subtitle2" className={styles['secondaryText']}>
-                    {`${expert.workCompany} • ${expert.workPosition}`}
+                    {`${workCompany} • ${workPosition}`}
                 </Typography>
             </VStack>
         </VStack>
