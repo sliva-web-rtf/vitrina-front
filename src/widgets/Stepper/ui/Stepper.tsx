ba12c1a5160ce8a39563501344788cf5868270f6
Nihat04 <aliev.nihat4@gmail.com>
2025-02-25 00:07:43 +0500

    fix: optimized stepper and better adaptive auth banner

--- src/widgets/Stepper/ui/Stepper.tsx
+++ src/widgets/Stepper/ui/Stepper.tsx
@@ -1,54 +1,19 @@
 import React from 'react';
-import {
-    Box,
-    Step,
-    StepConnector,
-    stepConnectorClasses,
-    StepIconProps,
-    StepLabel,
-    Stepper,
-    styled,
-} from '@mui/material';
+import { Step, stepConnectorClasses, Stepper, styled } from '@mui/material';
 
-const QontoConnector = styled(StepConnector)(({ theme }) => ({
+const StyledStepper = styled(Stepper)(() => ({
+    width: '100%',
+    gap: '21px',
     [`& .${stepConnectorClasses.line}`]: {
         display: 'none',
     },
 }));
 
-const QontoStepIconRoot = styled('div')<{ ownerState: { active?: boolean } }>(({ theme }) => ({
-    color: '#eaeaf0',
-    display: 'flex',
-    height: 22,
-    alignItems: 'center',
-    '& .QontoStepIcon-circle': {
-        width: 92,
-        height: 8,
-        borderRadius: '100px',
-        backgroundColor: 'currentColor',
-    },
-    ...theme.applyStyles('dark', {
-        color: theme.palette.grey[700],
-    }),
-    variants: [
-        {
-            props: ({ ownerState }) => ownerState.active,
-            style: {
-                color: '#784af4',
-            },
-        },
-    ],
+const StyledStep = styled(Step)<{ active?: boolean }>(({ active }) => ({
+    height: '8px',
+    backgroundColor: active ? '#784af4' : '#eaeaf0',
+    borderRadius: '100px',
 }));
-
-function QontoStepIcon(props: StepIconProps) {
-    const { active, completed, className } = props;
-
-    return (
-        <QontoStepIconRoot ownerState={{ active }} className={className}>
-            <div className="QontoStepIcon-circle" />
-        </QontoStepIconRoot>
-    );
-}
 
 interface LabellessStepperProps {
     currentStep: number;
@@ -59,14 +24,10 @@ export const LabellessStepper = (props: LabellessStepperProps) => {
     const { currentStep, stepsCount } = props;
 
     return (
-        <Box>
-            <Stepper alternativeLabel activeStep={currentStep} connector={<QontoConnector />}>
-                {Array.from({ length: stepsCount }).map((_, index) => (
-                    <Step key={index}>
-                        <StepLabel StepIconComponent={QontoStepIcon} />
-                    </Step>
-                ))}
-            </Stepper>
-        </Box>
+        <StyledStepper alternativeLabel activeStep={currentStep}>
+            {Array.from({ length: stepsCount }).map((_, index) => (
+                <StyledStep key={index} active={index === currentStep} />
+            ))}
+        </StyledStepper>
     );
 };
