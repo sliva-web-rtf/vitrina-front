7934dd48d6824bc39c7704809791cf27ed83c6cf
Nihat04 <aliev.nihat4@gmail.com>
2025-03-31 03:09:10 +0500

    feat: create profile page (without adaptive)

--- src/pages-components/ProfilePage/ui/ProfilePage.tsx
+++ src/pages-components/ProfilePage/ui/ProfilePage.tsx
@@ -1,8 +1,29 @@
-import { VStack } from '@/shared/ui';
+import styles from './ProfilePage.module.scss';
+
 import React from 'react';
 
+import { ProfileUser } from '@/entities/user';
+import { HStack, VStack } from '@/shared/ui';
+import { DefaultHeader } from '@/widgets/Header';
+import { ProfileBanner } from '@/widgets/ProfileBanner';
+import { ProjectsList } from '@/widgets/ProjectsList';
+import { Box } from '@mui/material';
+
 const ProfilePage = () => {
-    return <VStack></VStack>;
+    return (
+        <VStack>
+            <DefaultHeader />
+            <ProfileBanner />
+            <HStack spacing={8} padding={8} alignItems="start">
+                <Box className={styles['stepOnBannerWrapper']}>
+                    <Box className={styles['stepOnBanner']}>
+                        <ProfileUser />
+                    </Box>
+                </Box>
+                <ProjectsList />
+            </HStack>
+        </VStack>
+    );
 };
 
 export default ProfilePage;
