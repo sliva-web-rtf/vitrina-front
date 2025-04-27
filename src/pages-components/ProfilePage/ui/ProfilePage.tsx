dbb35fc7ae81502f4b6e169d744e2fc4b50bd824
Nihat04 <aliev.nihat4@gmail.com>
2025-04-27 19:51:28 +0500

    fix(header): remove header in layout and add header to pages

--- src/pages-components/ProfilePage/ui/ProfilePage.tsx
+++ src/pages-components/ProfilePage/ui/ProfilePage.tsx
@@ -7,10 +7,12 @@ import { HStack, VStack } from '@/shared/ui';
 import { ProfileBanner } from '@/widgets/ProfileBanner';
 import { ProjectsList } from '@/widgets/ProjectsList';
 import { Box } from '@mui/material';
+import { Header } from '@/widgets/Header';
 
 const ProfilePage = () => {
     return (
         <VStack>
+            <Header transparent />
             <ProfileBanner />
             <HStack gap={8} padding={8} alignItems="start" className={styles['smVertical']}>
                 <Box className={styles['stepOnBannerWrapper']}>
