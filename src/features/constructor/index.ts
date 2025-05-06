2e2f1367cde52f4dd81bce815f36f8c7b1960b15
Nihat04 <aliev.nihat4@gmail.com>
2025-05-06 22:41:19 +0500

    feat: done with constructor main page

--- src/features/constructor/index.ts
+++
@@ -1,1 +0,0 @@
-export { ConstructorTemplatesList } from './ui/ConstructorTemplatesList/ConstructorTemplatesList';
--- src/pages-components/ConstructorMainPage/ui/ConstructorMainPage.tsx
+++ src/pages-components/ConstructorMainPage/ui/ConstructorMainPage.tsx
@@ -1,11 +1,18 @@
-import { ConstructorTemplatesList } from '@/features/constructor';
-import { VStack } from '@/shared/ui';
 import React from 'react';
+
+import { Header } from '@/widgets/Header';
+import { ConstructorTemplatesList } from '@/widgets/ConstructorTemplatesList';
+import { ProjectsList } from '@/widgets/ProjectsList';
+import { VStack } from '@/shared/ui';
+import { Box } from '@mui/material';
 
 const ConstructorMainPage = () => {
     return (
         <VStack>
             <ConstructorTemplatesList />
+            <Box sx={{ backgroundColor: 'white', margin: '0 -20px' }}>
+                <ProjectsList />
+            </Box>
         </VStack>
     );
 };
