aa14b9d30f1f083239ecc129dde173a53608281c
Nihat04 <aliev.nihat4@gmail.com>
2025-05-06 22:41:50 +0500

    feat: create routes group for constructor

---
+++ app/constructor/page.tsx
@@ -0,0 +1,8 @@
+import ConstructorMainPage from '@/pages-components/ConstructorMainPage';
+import React from 'react';
+
+const Constructor = () => {
+    return <ConstructorMainPage />;
+};
+
+export default Constructor;
