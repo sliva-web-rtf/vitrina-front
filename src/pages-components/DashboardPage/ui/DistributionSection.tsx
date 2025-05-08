f7fa46f473e9b4ee75c084493893d3e9891a1c76
Aliev Nihat <119858790+Nihat04@users.noreply.github.com>
2025-05-08 18:32:33 +0500

    fix: change dashboard bar chart colors

--- src/pages-components/DashboardPage/ui/DistributionSection.tsx
+++ src/pages-components/DashboardPage/ui/DistributionSection.tsx
@@ -5,21 +5,22 @@ import { PieChart } from '@/shared/ui/PieChart/PieChart';
 import { DashboardBlock } from '@/shared/ui/DashboardBlock/DashboardBlock';
 import { VStack } from '@/shared/ui';
 
-const TITLE = "Распределение студентов по кураторам"
+const TITLE = 'Распределение студентов по кураторам';
 
-const SUBTITLE = "Всего студентами было создано 534 проекта, в том числе 318 проектов от УрФУ, 179 проектов от компаний-партнеров и 37 студенческих проектов. "
+const SUBTITLE =
+    'Всего студентами было создано 534 проекта, в том числе 318 проектов от УрФУ, 179 проектов от компаний-партнеров и 37 студенческих проектов. ';
 
 const CHART_DATA = [
-    { label: 'Студенческий проект', data: 37, backgroundColor: '#3F72E0' },
-    { label: 'Проект от партнера', data: 179, backgroundColor: '#5F8EFF' },
-    { label: 'Проект от Университета', data: 281, backgroundColor: '#88ADFF' },
+    { label: 'Студенческий проект', data: 37, backgroundColor: 'rgba(0, 123, 255, 1)' },
+    { label: 'Проект от партнера', data: 179, backgroundColor: 'rgba(0, 123, 255, 0.5)' },
+    { label: 'Проект от Университета', data: 281, backgroundColor: 'rgba(173, 216, 230, 1)' },
 ];
 
 export const DistributionSection = () => {
     return (
         <Box id="distribution">
             <DashboardBlock title={TITLE} subtitle={SUBTITLE}>
-                <VStack alignItems={"center"}>
+                <VStack alignItems={'center'}>
                     <PieChart data={CHART_DATA} />
                 </VStack>
             </DashboardBlock>
