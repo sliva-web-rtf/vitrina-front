3dbc7e9d09036fab053568098564137c93e20ddf
Aliev Nihat <119858790+Nihat04@users.noreply.github.com>
2025-05-25 19:31:29 +0500

    !refactor: reorgonise styles ti be splited into globla and layout specific

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import '@/app/styles/index.scss';
+import '@/app/styles/global.scss';
 
 import styles from './layout.module.scss';
 
--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -1,4 +1,4 @@
-import '@/app/styles/index.scss';
+import '@/app/styles/global.scss';
 
 import { Providers } from '@/app/providers';
 import { Stack } from '@mui/material';
