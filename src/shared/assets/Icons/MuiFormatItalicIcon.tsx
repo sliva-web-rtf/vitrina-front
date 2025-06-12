b3239445bcbe29a7bd3515de45a69a2e7d326994
Aliev Nihat <119858790+Nihat04@users.noreply.github.com>
2025-06-13 02:20:15 +0500

    fix: icons error

--- src/shared/assets/Icons/MuiFormatItalicIcon.tsx
+++ src/shared/assets/Icons/MuiFormatItalicIcon.tsx
@@ -4,20 +4,20 @@ const MuiFormatItalicIcon = () => (
     <SvgIcon>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M10 5.00026C10 4.44797 10.4477 4.00026 11 4.00026H17C17.5523 4.00026 18 4.44797 18 5.00026C18 5.55254 17.5523 6.00026 17 6.00026H11C10.4477 6.00026 10 5.55254 10 5.00026Z"
                 fill="#4D5A66"
             />
             <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M6 19.0003C6 18.448 6.44772 18.0003 7 18.0003H13C13.5523 18.0003 14 18.448 14 19.0003C14 19.5525 13.5523 20.0003 13 20.0003H7C6.44772 20.0003 6 19.5525 6 19.0003Z"
                 fill="#4D5A66"
             />
             <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M14.2747 4.03873C14.8058 4.19046 15.1132 4.74394 14.9615 5.27498L10.9615 19.275C10.8098 19.806 10.2563 20.1135 9.72528 19.9618C9.19424 19.8101 8.88675 19.2566 9.03848 18.7255L13.0385 4.72554C13.1902 4.1945 13.7437 3.88701 14.2747 4.03873Z"
                 fill="#4D5A66"
             />
