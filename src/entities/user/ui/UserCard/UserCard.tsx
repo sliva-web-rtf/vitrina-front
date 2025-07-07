e01dd22b2628d397acde3d399b2b64cce364ded7
Aliev Nihat <119858790+Nihat04@users.noreply.github.com>
2025-07-08 04:31:39 +0500

    fix: user profile

--- src/entities/user/ui/UserCard/UserCard.tsx
+++ src/entities/user/ui/UserCard/UserCard.tsx
@@ -1,29 +1,29 @@
+import { Typography } from '@mui/material';
+
 import { ellipsisTextStyle } from '@/shared/lib/const/ellipsisText';
 import { lowercased } from '@/shared/lib/helpers/lowercased';
 import { protocolless } from '@/shared/lib/helpers/protocolless';
 import { DeepLink, ProjectCardAvatar, VStack } from '@/shared/ui';
-import { Typography } from '@mui/material';
 import { User } from '../../model/types/user';
 import classNames from './UserCard.module.scss';
 import { UserRoles } from '../UserRoles';
 
 export const UserCard = (props: User) => {
-    const { firstName, lastName, patronymic, description, email, roles } = props;
+    const { email } = props;
     const lowercasedEmail = lowercased(email);
 
     return (
         <VStack className={classNames.card}>
-            {/* <Image priority={true} src={image || defaultUserImage} alt={`${lastName} ${firstName} ${patronymic}`} /> */}
             <ProjectCardAvatar className={classNames.image} />
             <VStack className={classNames.content}>
-                <UserRoles roles={roles} />
+                {/* <UserRoles roles={roles} /> */}
 
                 <VStack spacing={1}>
                     <Typography variant="h5">
-                        {lastName} {firstName} <br />
-                        {patronymic}
+                        Иванов Иван <br />
+                        Иванович
                     </Typography>
-                    <Typography>{description}</Typography>
+                    <Typography>Какое-то описание</Typography>
                 </VStack>
 
                 <DeepLink link={lowercasedEmail}>
